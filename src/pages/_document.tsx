a38418c241c8174cb5f46c757fb61f052d4c6c62
Ton Almeida <oi@tonalmeida.dev>
2023-02-22 23:47:50 -0300

    remove microsoft-validate

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,10 +17,6 @@ export default class MyDocument extends Document {
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta httpEquiv="cache-control" content="public" />
           <meta
-            name="msvalidate.01"
-            content={process.env.MICROSOFT_VALIDATE_01}
-          />
-          <meta
             name="keywords"
             content="sites, web, desenvolvedor, developer, front end, javascript, typescript, react, nextjs, seo, css, tailwind, stitches, radix ui, motion, ux ui design"
           />
